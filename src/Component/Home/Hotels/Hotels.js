32fcdea5ca831a545527d8916b5626949e9ae779
rokibulslam <rokibulslam6@gmail.com>
2021-10-30 12:35:52 +0600

    get api created

--- src/Component/Home/Hotels/Hotels.js
+++ src/Component/Home/Hotels/Hotels.js
@@ -7,7 +7,7 @@ const Hotels = () => {
     const [Hotels, setHotels] = useState([])
 
     useEffect(() => {
-        fetch("./services.json")
+        fetch("http://localhost:5000/hotels")
             .then(res => res.json())
             .then(data=>setHotels(data))
     }, [])
@@ -20,19 +20,19 @@ const Hotels = () => {
                 <h1 className="text-center p-5">Our Promise For A New Kind Of Caring</h1>
             <div className="row">
             {
-                Hotels?.map(service=>
+                Hotels?.map(hotel=>
                     <div
-                        key={service._id}
+                        key={hotel._id}
                         className="col-md-3 g-4">
                     <Card className="h-100 w-100 card-style">
-                    <Card.Img className="img-fluid" variant="top" src={service.imageURL} />
+                    <Card.Img className="img-fluid" variant="top" src={hotel.img} />
                     <Card.Body className="d-flex flex-column justify-content-end rounded card-text">
-                        <Card.Title className="fw-bold">{service.name}</Card.Title>
+                        <Card.Title className="fw-bold">{hotel.name}</Card.Title>
                         <Card.Text>
-                        {service.description.slice(0,100)}
+                        {hotel.Description.slice(0, 100)}
                         </Card.Text>
-                        <Link to={`/service/${service._id}`}>
-                            <Button className="btn-grad">Details</Button>
+                        <Link to={`/service/${hotel._id}`}>
+                            <Button className="btn-grad">Book Now</Button>
                         </Link>
                     </Card.Body>
                     </Card>
