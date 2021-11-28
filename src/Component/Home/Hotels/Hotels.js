a976f036a986e2aae10c551d68f215a8d6791f78
rokibulslam <rokibulslam6@gmail.com>
2021-11-28 19:50:05 +0600

    navbar changed

--- src/Component/Home/Hotels/Hotels.js
+++ src/Component/Home/Hotels/Hotels.js
@@ -15,32 +15,33 @@ const Hotels = () => {
 
 
     return (
-        <div className="bg-color">
-            <div id="hotels" className="container py-5 ">
-                <h1 className="text-center p-5 info-text fs-1 fw-bolder">Let's The Journey Begin....</h1>
-            <div className="row">
-            {
-                Hotels?.map(hotel=>
-                    <div
-                        key={hotel._id}
-                        className="col-md-3 g-4">
-                    <Card className="h-100 w-100 card-style">
-                    <Card.Img className="img-fluid" variant="top" src={hotel.img} />
-                    <Card.Body className="d-flex flex-column justify-content-end rounded card-text">
-                        <Card.Title className="fw-bold">{hotel.name}</Card.Title>
-                        <Card.Text>
-                        {hotel.Description.slice(0, 100)}
-                        </Card.Text>
-                        <Link to={`/placeOrder/${hotel._id}`}>
-                            <Button className="btn-grad">Book Now</Button>
-                        </Link>
-                    </Card.Body>
-                    </Card>
-                </div>)
-            }
-            </div>
+      <div className="bg-color">
+        <div id="hotels" className="container py-5 ">
+          <h1 className="text-center p-5  fs-1 fw-bolder">
+            Inspiration for your next trip.
+          </h1>
+          <div className="row">
+            {Hotels?.map((hotel) => (
+              <div key={hotel._id} className="col-md-3 g-4 col-sm-12">
+                <Card className="h-100 w-100 card-style">
+                  <Card.Img
+                    className="img-fluid"
+                    variant="top"
+                    src={hotel.img}
+                  />
+                  <Card.Body className="d-flex flex-column justify-content-end rounded card-text">
+                    <Card.Title className="fw-bold">{hotel.name}</Card.Title>
+                    <Card.Text>{hotel.Description.slice(0, 100)}</Card.Text>
+                    <Link to={`/placeOrder/${hotel._id}`}>
+                      <Button className="btn-grad">Book Now</Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+          </div>
         </div>
-        </div>
+      </div>
     );
 };
 
--- src/Component/Home/NavBar/NavBar.js
+++ src/Component/Home/NavBar/NavBar.js
@@ -10,24 +10,24 @@ const NavBar = () => {
     
     return (
         <div>
-            <Navbar className="nav-color sticky-top overflow-hidden navbar " variant="dark" collapseOnSelect expand="lg" >
+            <Navbar className="nav-color sticky-top overflow-hidden navbar " variant="light" collapseOnSelect expand="lg" >
             <Container>
-                    <Navbar.Brand className="fw-bolder text-white"><img className="rounded-3" alt="" />TAdvisor</Navbar.Brand>
+                    <Navbar.Brand className="fw-bolder "><img className="rounded-3" alt="" />TAdvisor</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
-                <Nav.Link as={HashLink} className="text-decoration-none fw-normal text-white"  to="/home#home">Home</Nav.Link>
-                <Nav.Link as={HashLink} className="text-decoration-none fw-normal px-2 text-white"  to="/home#hotels">Services</Nav.Link>
-                {user?.email ? <Nav.Link as={HashLink} className="text-decoration-none  fw-normal px-2 text-white" to="/myOrders">My Order</Nav.Link>: ''}
-                {user?.email ? <Nav.Link as={HashLink} className="text-decoration-none fw-normal px-2  text-white" to="/admin">Admin Dashboard</Nav.Link> : ''}
+                <Nav.Link as={HashLink} className="text-decoration-none fw-bold text-black"  to="/home#home">Home</Nav.Link>
+                <Nav.Link as={HashLink} className="text-decoration-none fw-bold px-2 text-black"  to="/home#hotels">Services</Nav.Link>
+                {user?.email ? <Nav.Link as={HashLink} className="text-decoration-none  fw-bold px-2 text-black" to="/myOrders">My Order</Nav.Link>: ''}
+                {user?.email ? <Nav.Link as={HashLink} className="text-decoration-none fw-bold px-2  text-black" to="/admin">Admin Dashboard</Nav.Link> : ''}
                 
-                <Nav.Link as={HashLink} className="text-decoration-none px-2  fw-normal text-white" to="/about">About Us</Nav.Link>
+                <Nav.Link as={HashLink} className="text-decoration-none px-2  fw-bold text-black" to="/about">About Us</Nav.Link>
                 </Nav>
                 <Nav className="justify-content-center align-items-center">
-                {user.email ? <button className="button text-white p-2"  onClick={logOut}>Sign Out</button> :
-                <Nav.Link as={HashLink} className="button text-white m-2" to="/signIn">Sign in</Nav.Link>}
-                {user?.email ? <p className="mb-0 ps-3 text-white"> {user.displayName}</p> 
-                                : <Nav.Link as={HashLink} className="button text-white" to="/signUp">Sign Up</Nav.Link>}
+                {user.email ? <button className="button text-black p-2"  onClick={logOut}>Sign Out</button> :
+                <Nav.Link as={HashLink} className="button text-black m-2" to="/signIn">Sign in</Nav.Link>}
+                {user?.email ? <p className="mb-0 ps-3 text-black"> {user.displayName}</p> 
+                                : <Nav.Link as={HashLink} className="button text-black" to="/signUp">Sign Up</Nav.Link>}
                 </Nav>
             </Navbar.Collapse>
             </Container>
